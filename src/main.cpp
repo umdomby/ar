d89fbb8b25f91946597d797c09d67f55f6fae93f
Navatar <umdomby@yandex.ru>
2025-03-25 15:41:42 +0300

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoWebsockets.h>
@@ -8,12 +9,13 @@ using namespace websockets;
 const char* ssid = "Robolab124";
 const char* password = "wifi123123123";
 const char* websocket_server = "ws://192.168.0.151:8080";
-const char* deviceId = "123"; // Должен совпадать с ID в клиенте
+const char* deviceId = "123";
 
 WebsocketsClient client;
 unsigned long lastReconnectAttempt = 0;
-unsigned long lastLogTime = 0;
 unsigned long lastHeartbeatTime = 0;
+unsigned long lastLogTime = 0;
+bool wasConnected = false;
 
 void sendLogMessage(const char* message) {
   DynamicJsonDocument doc(256);
@@ -25,10 +27,20 @@ void sendLogMessage(const char* message) {
   client.send(output);
 }
 
+void sendCommandAck(const char* command) {
+  DynamicJsonDocument doc(128);
+  doc["type"] = "command_ack";
+  doc["command"] = command;
+  doc["deviceId"] = deviceId;
+  String output;
+  serializeJson(doc, output);
+  client.send(output);
+}
+
 void onMessageCallback(WebsocketsMessage message) {
   Serial.print("Received: ");
   Serial.println(message.data());
-  sendLogMessage("Received command from server");
+  sendLogMessage("Command received");
 
   DynamicJsonDocument doc(256);
   DeserializationError error = deserializeJson(doc, message.data());
@@ -38,18 +50,21 @@ void onMessageCallback(WebsocketsMessage message) {
     if (command) {
       if (strcmp(command, "forward") == 0) {
         Serial.println("Executing: FORWARD");
-        sendLogMessage("Executing FORWARD command");
+        sendLogMessage("Executing FORWARD");
+        sendCommandAck("forward");
       } 
       else if (strcmp(command, "backward") == 0) {
         Serial.println("Executing: BACKWARD");
-        sendLogMessage("Executing BACKWARD command");
+        sendLogMessage("Executing BACKWARD");
+        sendCommandAck("backward");
       } 
       else if (strcmp(command, "servo") == 0) {
         int angle = doc["params"]["angle"];
         Serial.printf("Setting servo angle to: %d\n", angle);
         char logMsg[50];
-        snprintf(logMsg, sizeof(logMsg), "Setting servo angle to %d", angle);
+        snprintf(logMsg, sizeof(logMsg), "Servo set to %d", angle);
         sendLogMessage(logMsg);
+        sendCommandAck("servo");
       }
     }
   }
@@ -59,8 +74,9 @@ void onEventsCallback(WebsocketsEvent event, String data) {
   if (event == WebsocketsEvent::ConnectionOpened) {
     Serial.println("Connected to server!");
     sendLogMessage("ESP connected to server");
+    wasConnected = true;
     
-    // Отправляем тип клиента
+    // Send client type
     DynamicJsonDocument typeDoc(128);
     typeDoc["type"] = "client_type";
     typeDoc["clientType"] = "esp";
@@ -68,7 +84,7 @@ void onEventsCallback(WebsocketsEvent event, String data) {
     serializeJson(typeDoc, typeOutput);
     client.send(typeOutput);
     
-    // Идентификация устройства
+    // Identify device
     DynamicJsonDocument doc(128);
     doc["type"] = "identify";
     doc["deviceId"] = deviceId;
@@ -76,7 +92,7 @@ void onEventsCallback(WebsocketsEvent event, String data) {
     serializeJson(doc, output);
     client.send(output);
 
-    // Уведомляем о подключении ESP
+    // Send connection status
     DynamicJsonDocument statusDoc(128);
     statusDoc["type"] = "esp_status";
     statusDoc["status"] = "connected";
@@ -84,44 +100,47 @@ void onEventsCallback(WebsocketsEvent event, String data) {
     String statusOutput;
     serializeJson(statusDoc, statusOutput);
     client.send(statusOutput);
+    Serial.println("Sent ESP status: connected");
   }
   else if (event == WebsocketsEvent::ConnectionClosed) {
     Serial.println("Connection closed");
-    
-    // Попытка уведомить сервер об отключении
-    if (client.available()) {
-      DynamicJsonDocument statusDoc(128);
-      statusDoc["type"] = "esp_status";
-      statusDoc["status"] = "disconnected";
-      statusDoc["deviceId"] = deviceId;
-      String statusOutput;
-      serializeJson(statusDoc, statusOutput);
-      client.send(statusOutput);
+    if (wasConnected) {
+      wasConnected = false;
+      if (client.available()) {
+        DynamicJsonDocument statusDoc(128);
+        statusDoc["type"] = "esp_status";
+        statusDoc["status"] = "disconnected";
+        statusDoc["deviceId"] = deviceId;
+        statusDoc["reason"] = "connection closed";
+        String statusOutput;
+        serializeJson(statusDoc, statusOutput);
+        client.send(statusOutput);
+      }
     }
   }
   else if (event == WebsocketsEvent::GotPing) {
-    Serial.println("Got a Ping!");
-  }
-  else if (event == WebsocketsEvent::GotPong) {
-    Serial.println("Got a Pong!");
+    client.pong();
   }
 }
 
 void setup() {
   Serial.begin(115200);
   
+  // Connect to WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nConnected to WiFi");
-  Serial.print("IP address: ");
+  Serial.println("\nWiFi connected");
+  Serial.print("IP: ");
   Serial.println(WiFi.localIP());
 
+  // Setup WebSocket
   client.onMessage(onMessageCallback);
   client.onEvent(onEventsCallback);
   
+  // Connect to server
   if (client.connect(websocket_server)) {
     Serial.println("WebSocket connected");
   } else {
@@ -135,25 +154,26 @@ void loop() {
       lastReconnectAttempt = millis();
       if (client.connect(websocket_server)) {
         Serial.println("Reconnected to WebSocket");
-        sendLogMessage("ESP reconnected to server");
+        sendLogMessage("ESP reconnected");
       }
     }
   } else {
     client.poll();
     
-    // Отправка heartbeat каждые 10 секунд
-    if (millis() - lastHeartbeatTime > 5000) {
+    // Send heartbeat every 10 seconds
+    if (millis() - lastHeartbeatTime > 1000) {
       lastHeartbeatTime = millis();
-      sendLogMessage("ESP heartbeat - system normal");
+      sendLogMessage("Heartbeat - system OK");
     }
     
-    // Пример отправки системных логов
+    // Send system info every 30 seconds
     if (millis() - lastLogTime > 10000) {
       lastLogTime = millis();
       float voltage = analogRead(A0) * 3.3 / 1024.0;
-      char voltageMsg[50];
-      snprintf(voltageMsg, sizeof(voltageMsg), "System status - Voltage: %.2fV", voltage);
-      sendLogMessage(voltageMsg);
+      char sysMsg[50];
+      snprintf(sysMsg, sizeof(sysMsg), "System: %.2fV, %ddBm", 
+               voltage, WiFi.RSSI());
+      sendLogMessage(sysMsg);
     }
   }
 }
