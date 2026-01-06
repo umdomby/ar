26f2c730767eaa24c0b96119ddba0a33d69bf37b
umdomby <umdomby@yandex.ru>
2026-01-06 13:48:24 +0300

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ using namespace websockets;
 const char *ssid = "Robolab124";
 //const char *ssid = "p8";
 const char *password = "wifi123123123";
-const char *websocket_server = "wss://ardua.site:444/wsar";
+const char *websocket_server = "wss://a.ardu.live:444/wsar";
 
 String alarm = "off"; 
 boolean alarmMotion = false;
